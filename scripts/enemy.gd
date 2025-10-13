49d260b4456705de849f99a652e7aed8cd2e83f2
Tassnaz <tassnass2018@gmail.com>
2025-10-13 13:46:56 +0200

    Pathfinding

---
+++ scripts/enemy.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+# Variabel för att definera "Target"
+@export var PathToTarget = NodePath()
+
+# Variabler för hastighet och rörelse
+var Velocity = Vector2.ZERO
+@export var MaxSpeed = 280
+var Acceleration = 8.0
+
+# Kopplingar till olika noder
+@onready var Target = get_node(PathToTarget)
+@onready var NavAgent = $NavigationAgent2D
+@onready var UpdatePathTimer = get_node("UpdateTimer")
+
+func _ready():
+	
+	# Talar om vart "Target" är
+	NavAgent.set_target_position(Target.global_position)
+	
+	# Kopplar timern med kod samt sätter igång den
+	UpdatePathTimer.timeout.connect(updatePath)
+	UpdatePathTimer.start()
+
+func updatePath():
+	
+	NavAgent.set_target_position(Target.global_position)
+	UpdatePathTimer.start()
+	
+func _physics_process(delta):
+	
+	# Fixar så att enemy inte "gungar" när den är framme
+	if NavAgent.is_navigation_finished():
+		return
+		
+	var Direction = global_position.direction_to(NavAgent.get_next_path_position())
+	var TargetVelocity = Direction * MaxSpeed
+	var Steering = (TargetVelocity - velocity) * delta * Acceleration
+	
+	# Ökar velocity baserat på ekvationen ovan
+	velocity += Steering
+	
+	# Rör Enemy
+	Velocity = move_and_slide()
