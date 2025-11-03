908a302d5260f618a98e8428cae9d0933ba7e831
Tassnaz <tassnass2018@gmail.com>
2025-11-03 14:33:41 +0100

    Topdown movement

--- scripts/target.gd
+++ scripts/target.gd
@@ -1,19 +1,43 @@
 extends CharacterBody2D
 
 
-var CanGrab = false
+# Värden
+@export var max_speed = 400
+@export var acceleration = 1000
+@export var friction = 1500
 
-var Offset = Vector2()
-
-func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		CanGrab = event.pressed
+func _physics_process(delta):
+	# Movement
+	
+	# Hämta Input
+	var direction = Input.get_vector("Left", "Right", "Up", "Down")
+	
+	# OM man går i en riktning accelerera tills man når max_speed
+	if direction != Vector2.ZERO:
+		var target_velocity = direction * max_speed
+		velocity = velocity.move_toward(target_velocity, acceleration * delta)
 		
-		Offset = position - get_global_mouse_position()
-
-func _process(_delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && CanGrab:
-		position = get_global_mouse_position() + Offset
+	# Sakta ner om man inte ger någon input
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	move_and_slide()
+	
+	# Animationer
+	if Input.is_action_pressed("Left") && !Input.is_action_pressed("Right"):
+		$AnimationPlayer.play("Left")
+	
+	elif Input.is_action_pressed("Right") && !Input.is_action_pressed("Left"):
+		$AnimationPlayer.play("Right")
+	
+	elif Input.is_action_pressed("Up"):
+		$AnimationPlayer.play("Up")
+	
+	elif Input.is_action_pressed("Down"):
+		$AnimationPlayer.play("Down")
+	
+	else: 
+		$AnimationPlayer.stop()
 
 
 func _on_switch_target_hitbox_body_entered(body):
