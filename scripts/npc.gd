99de91067d22e4e43346520ecebd66be31b43499
Arreb <tassnass2018@gmail.com>
2026-01-22 21:13:41 +0100

    dialog, ny bana

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -7,22 +7,39 @@ var max_dialog: int = 0
 
 @export var dialog: Dictionary = {
 	
-	1: "Hej",
+	1: "Hello",
 	
-	2: "Jag är en trolkarl",
+	2: "I am a wizzard",
 }
+
 
 func _ready():
 	$DialogBox.visible = false
 	max_dialog = dialog.size()
+	
+	
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("Interact") && is_interacting_with == true:
+		talked_to()
 
 
-
+func talked_to():
+	if dialog_counter <= max_dialog:
+		Global.current_dialog = dialog.get(dialog_counter)
+		
+		dialog_counter += 1
+		$DialogBox.visible = true
+		
+	elif dialog_counter > max_dialog:
+		$DialogBox.visible = false
+		dialog_counter = 1
+	
+	
 func _on_body_entered(body):
 	if body.name == "Player":
 		is_interacting_with = true
 	
-
+	
 func _on_body_exited(body):
 	if body.name == "Player":
 		Global.current_dialog = ""
