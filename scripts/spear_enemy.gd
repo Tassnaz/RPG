7771d65b23b8b9ec5609fe4ed83287385c968c56
Arreb <tassnass2018@gmail.com>
2026-05-27 21:50:49 +0200

    feat: add enemy pathfinding

--- scripts/spear_enemy.gd
+++ scripts/spear_enemy.gd
@@ -2,26 +2,44 @@ extends CharacterBody2D
 
 var HP: int = 100
 var knockback_velocity: Vector2 = Vector2.ZERO
+var player: Node2D
+var player_detected: bool = false
 
 @export var knockback_friction: float = 10.0
+@export var speed: float = 100.0
+
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 
 
 func _ready():
-	pass
+	player = get_tree().get_first_node_in_group("players")
 
 func _physics_process(delta):
-	
+	# Uppdates health bar
 	$HealthBar.value = HP
 	
+	if player == null:
+		return
+		
+	if player_detected:
+		# Tells the NavAgent where to go
+		nav_agent.target_position = player.global_position
+		
+		# Gets the next point along the path
+		var next_point = nav_agent.get_next_path_position()
+		var direction = global_position.direction_to(next_point)
+	
+		velocity = (direction * speed) + knockback_velocity
+	
+	# Handles death
 	if HP <= 0:
 		queue_free()
+		return
 	
 	knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, knockback_friction * delta * 100)
 	
-	velocity = knockback_velocity
-	
 	move_and_slide()
-	
+
 
 func apply_knockback(force: Vector2):
 	knockback_velocity = force
@@ -35,5 +53,8 @@ func _on_hit_box_area_entered(area):
 		
 		HP = HP - area.get("Damage")
 		print ("Enemy hp is ", HP)
-		
-		
+
+
+func _on_player_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("players"):
+		player_detected = true
